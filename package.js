62e2a1d9ce182e2b1c8e3875ca28a35c3067d562
Artem Polikarpov <artpolikarpov@gmail.com>
2015-12-18 02:47:00 +0300

    0.7.1 → 0.7.2

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: 'chatra:redpubsub',
-  version: '0.7.1',
+  version: '0.7.2',
   // Brief, one-line summary of the package.
   summary: 'Custom pub/sub interface for Meteor on top of Redis',
   // URL to the Git repository containing the source code for this package.
   git: 'https://github.com/chatr/redpubsub.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
-  documentation: null
+  documentation: 'README.md'
 });
 
 Npm.depends({
