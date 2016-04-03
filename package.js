a3c587004454c1ee5c3b3337775a14e2eb23d580
ggn06awu <mark.beeby@gmail.com>
2016-04-03 14:24:34 +0100

    Update package.js
    Correcting the GIT source.

--- package.js
+++ package.js
@@ -4,7 +4,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Custom pub/sub interface for Meteor on top of Redis',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/chatr/redpubsub.git',
+  git: 'https://github.com/ggn06awu/redpubsub.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
