fc169d00c70b1d109bd4f4ec7a23c933d67e5f88
Artem Polikarpov <artpolikarpov@gmail.com>
2017-02-01 21:20:57 +0300

    0.9.1

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'chatra:redpubsub',
-  version: '0.9.0',
+  version: '0.9.1',
   // Brief, one-line summary of the package.
   summary: 'Custom pub/sub interface for Meteor on top of Redis',
   // URL to the Git repository containing the source code for this package.
