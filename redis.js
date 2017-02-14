3d31202a297bd901d697a7a8c891dfac1552656c
Artem Polikarpov <artpolikarpov@gmail.com>
2017-02-14 23:51:26 +0300

    implementing `retry_strategy`

--- redis.js
+++ redis.js
@@ -10,7 +10,22 @@ var createRedisClient = function (conf, key, revive) {
     console.info(logLabel + 'connecting to Redis...', redisConfToString(conf));
 
     var client = Redis.createClient(conf.port, conf.host, {
-        retry_max_delay: 1000 * 10
+        retry_strategy: function (options) {
+            //if (options.error && options.error.code === 'ECONNREFUSED') {
+            //    // End reconnecting on a specific error and flush all commands with a individual error
+            //    return new Error('The server refused the connection');
+            //}
+            //if (options.total_retry_time > 1000 * 60 * 60) {
+            //    // End reconnecting after a specific timeout and flush all commands with a individual error
+            //    return new Error('Retry time exhausted');
+            //}
+            //if (options.times_connected > 10) {
+            //    // End reconnecting with built in error
+            //    return undefined;
+            //}
+            // reconnect after
+            return Math.min(options.attempt * 100, 3000);
+        }
     });
 
     if (conf.auth) {
