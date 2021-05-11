6e67bd67ae093b0080b076cdb84604400be5b558
artpolikarpov <artpolikarpov@gmail.com>
2021-05-11 10:54:14 +0300

    Publish v0.13.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'chatra:redpubsub',
-    version: '0.10.0',
+    version: '0.13.0',
     summary: 'Custom pub/sub interface for Meteor on top of Redis',
     git: 'https://github.com/chatr/redpubsub.git',
     documentation: 'README.md'
