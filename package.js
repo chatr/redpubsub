84cc49791318cd358537e2d3ab4ead490072329f
Petr Shardakov <dentist@onetwo.tv>
2025-11-21 16:01:55 +0100

    Release 1.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'chatra:redpubsub',
-    version: '1.0.2',
+    version: '1.0.3',
     summary: 'Custom pub/sub interface for Meteor on top of Redis',
     git: 'https://github.com/chatr/redpubsub.git',
     documentation: 'README.md',
